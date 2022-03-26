33bb73e4264d3116ac61d89373b3bc748a3e83f9
Lanx Xian <95699215+LL-Zephyr@users.noreply.github.com>
2022-03-26 15:50:57 +0800

    Create s_lib.py

---
+++ s_lib.py
@@ -0,0 +1,17 @@
+def numbers():
+  return "0123456789"
+
+def lower():
+  return "abcdefghijklmnopqrstuvwxyz"
+
+def upper():
+  return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def up_low():
+  return "abcdefghijklmnopqrstuvwxyz"+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def symbols():
+  return '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+  
+def space():
+  return " "
